f6f19471c3a95b41d2eeea8dc8cac5abe18bcbc1
SuppliedOrange <70258998+SuppliedOrange@users.noreply.github.com>
2022-01-22 09:40:43 +0530

    Update and rename installer3.bat to installer.bat

---
+++ Batch Files/installers/installer.bat
@@ -0,0 +1,8 @@
+cd..
+cd..
+python -m pip install -U pip
+python -m pip install -r requirements.txt
+python3 -m pip install -U pip
+python3 -m pip install -r requirements.txt
+py -m pip install -U pip
+py -m pip install -r requirements.txt
--- Batch Files/installers/installer3.bat
+++
@@ -1,4 +0,0 @@
-cd..
-cd..
-py -m pip install -U pip
-py -m pip install -r requirements.txt
